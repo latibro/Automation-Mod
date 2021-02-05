0e2d781ff31f71376bfce74a9513fba7ab0b4389
Latibro <mail@latibro.com>
2021-02-05 17:34:38 +0100

    Yet another try to fix problem with world.getLoadedEntities()

--- src/main/groovy/latibro/automation/nativeimpl/context/entity/collection/NativeServerLoadedEntityCollectionContext.groovy
+++ src/main/groovy/latibro/automation/nativeimpl/context/entity/collection/NativeServerLoadedEntityCollectionContext.groovy
@@ -1,10 +1,14 @@
 package latibro.automation.nativeimpl.context.entity.collection
 
 import com.google.common.base.Predicate
+import groovy.transform.CompileStatic
 import latibro.automation.core.context.CoreContext
 import latibro.automation.nativeimpl.context.server.NativeServerContext
 import net.minecraft.entity.Entity
 
+import javax.annotation.Nullable
+
+@CompileStatic
 final class NativeServerLoadedEntityCollectionContext extends AbstractNativeEntityCollectionContext implements CoreContext {
 
     private final NativeServerContext serverContext
@@ -15,7 +19,12 @@ final class NativeServerLoadedEntityCollectionContext extends AbstractNativeEnti
 
     Collection<Entity> getNativeEntityCollection() {
         //return (Collection<Entity>) serverContext.nativeServer.worlds.findResults { it }.collect { it.@loadedEntityList }.flatten()
-        return (Collection<Entity>) serverContext.nativeServer.worlds.findResults { it }.collect { it.getEntities(Entity, {true} as Predicate<Entity>) }.flatten()
+        return (Collection<Entity>) serverContext.nativeServer.worlds.findResults { it }.collect { it.getEntities(Entity.class, new Predicate<Entity>() {
+            @Override
+            boolean apply(@Nullable Entity input) {
+                return true
+            }
+        }) }.flatten()
     }
 
 }
--- src/main/groovy/latibro/automation/nativeimpl/context/entity/collection/NativeWorldLoadedEntityCollectionContext.groovy
+++ src/main/groovy/latibro/automation/nativeimpl/context/entity/collection/NativeWorldLoadedEntityCollectionContext.groovy
@@ -1,10 +1,14 @@
 package latibro.automation.nativeimpl.context.entity.collection
 
 import com.google.common.base.Predicate
+import groovy.transform.CompileStatic
 import latibro.automation.core.context.CoreContext
 import latibro.automation.nativeimpl.context.world.NativeWorldContext
 import net.minecraft.entity.Entity
 
+import javax.annotation.Nullable
+
+@CompileStatic
 final class NativeWorldLoadedEntityCollectionContext extends AbstractNativeEntityCollectionContext implements CoreContext {
 
     private final NativeWorldContext worldContext
@@ -15,7 +19,12 @@ final class NativeWorldLoadedEntityCollectionContext extends AbstractNativeEntit
 
     Collection<Entity> getNativeEntityCollection() {
         //return worldContext.nativeWorld.@loadedEntityList
-        return worldContext.nativeWorld.getEntities(Entity, { true } as Predicate<Entity>)
+        return worldContext.nativeWorld.getEntities(Entity.class, new Predicate<Entity>() {
+            @Override
+            boolean apply(@Nullable Entity input) {
+                return true
+            }
+        })
     }
 
 }
